3c6e4875d55f907877a59e1f390960dddaf0d232
daniel <daniel@localhost>
2013-07-12 17:14:59 +0000

    Added last element pointer to symbol table.

--- symboltable.c
+++ symboltable.c
@@ -1,13 +1,64 @@
 #include <stdlib.h>
+#include <string.h>
 #include "symboltable.h"
 
 SymbolTable initSymbolTable()
 {
     SymbolTable table;
     table.count = 0;
-    table.table = NULL;
+    table.root = NULL;
+    table.last = NULL;
     return table;
 }
-void insertSymbol(ptrSymbolTable table, char *symbol, int value);
-ptrSymbolTableEntry findSymbol(ptrSymbolTable table, char *symbol);
-void freeSymbolTable();
+
+void insertSymbol(ptrSymbolTable table, char *symbol, int value)
+{
+    ptrSymbolTableEntry entry;
+    
+    entry = (ptrSymbolTableEntry)malloc(sizeof(SymbolTableEntry));
+    entry->next = NULL;
+    entry->value = value;
+    
+    strcpy(entry->symbol, symbol);
+    
+    if (table->root == NULL)
+    {
+        table->root = entry;
+        table->last = entry;
+    }
+    else
+    {
+        table->last->next = entry;
+        table->last = entry;
+    }
+}
+
+ptrSymbolTableEntry findSymbol(ptrSymbolTable table, char *symbol)
+{
+    ptrSymbolTableEntry entry = table->root;
+    while (entry != NULL)
+    {
+        if (strcmp(entry->symbol, symbol) == 0) return entry;
+        entry = entry->next;
+    }
+    return NULL;
+}
+
+void freeSymbolTable(ptrSymbolTable table)
+{
+    void freeSymbolTableEntry(ptrSymbolTableEntry);
+    ptrSymbolTableEntry entry = table->root;
+    ptrSymbolTableEntry next = entry;
+    
+    while (entry != NULL)
+    {
+        next = entry->next;
+        freeSymbolTableEntry(entry);
+        entry = next;
+    }
+}
+
+void freeSymbolTableEntry(ptrSymbolTableEntry tableEntry)
+{
+    free(tableEntry);
+}
--- symboltable.h
+++ symboltable.h
@@ -5,22 +5,25 @@
  * Created on July 12, 2013, 2:44 PM
  */
 
+#include "consts.h"
+
 #ifndef SYMBOLTABLE_H
 #define	SYMBOLTABLE_H
 
 typedef struct tSymbolTableEntry{
-    char *symbol;
+    char symbol[MAX_LABEL_LENGTH]; /* labels are no longer than 30 chars */
     int value;
     struct tSymbolTableEntry *next;
 } SymbolTableEntry, *ptrSymbolTableEntry;
 
 typedef struct {
     int count;
-    ptrSymbolTableEntry table;
+    ptrSymbolTableEntry root;
+    ptrSymbolTableEntry last;
 } SymbolTable, *ptrSymbolTable;
 
 SymbolTable initSymbolTable();
 void insertSymbol(ptrSymbolTable table, char *symbol, int value);
 ptrSymbolTableEntry findSymbol(ptrSymbolTable table, char *symbol);
-void freeSymbolTable();
+void freeSymbolTable(ptrSymbolTable table);
 #endif	/* SYMBOLTABLE_H */
